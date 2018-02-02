b0f703caabe8b7ddaea76d67088ab5abacf24fef
seraco <sebastiaramon_1994@hotmail.com>
2018-02-02 14:51:56 +0100

    little modification

--- buildInitalA.m
+++ buildInitalA.m
@@ -1,4 +1,4 @@
-function A = build_inital_A(n)
+function A = buildInitalA(n)
 % The function builds an initial matrix of the form
 %       A=[
 %            1  0  0  0  0  1
@@ -11,15 +11,15 @@ function A = build_inital_A(n)
 % n - the size of the matrix A
 
 for i=1:n
-for j=1:n
-if(j==n)
-A(i,j)=1.0;
-elseif(i==j)
-A(i,j)=1.0;
-elseif(i>j)
-A(i,j)=-1.0;
-else
-A(i,j)=0.0;
-end
-end
+    for j=1:n
+        if(j==n)
+            A(i,j)=1.0;
+        elseif(i==j)
+            A(i,j)=1.0;
+        elseif(i>j)
+            A(i,j)=-1.0;
+        else
+            A(i,j)=0.0;
+        end
+    end
 end
